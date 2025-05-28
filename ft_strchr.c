a429a93190b7d02a87b52730ebd6b69e264a92d8
Yusuf Talha Sen <yussen@student.42istanbul.com.tr>
2025-05-28 20:16:06 +0300

    first commit

---
+++ ft_strchr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 18:27:05 by yussen            #+#    #+#             */
+/*   Updated: 2025/05/28 20:00:02 by yussen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (char *)(s + i);
+		i++;
+	}
+	return (0);
+}
+int	main(void)
+{
+		printf("%s\n", strchr("cba", 'b'));
+}
