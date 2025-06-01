c3dd44d1ebde99d84d5c4e3b69f4313840fc4ebe
Yusuf Talha Sen <yussen@student.42istanbul.com.tr>
2025-06-01 14:54:42 +0300

    first commit

---
+++ ft_strnstr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/01 12:02:26 by yussen            #+#    #+#             */
+/*   Updated: 2025/06/01 14:51:41 by yussen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	is_match(char *s1, char *s2)
+{
+	int	i;
+	
+	i = 0;
+	while (s2[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	int	i;
+	size_t	l_len;
+
+	i = 0;
+	l_len = ft_strlen((char *)little);
+	while (i <= len)
+	{
+		if (big[i] == little[i] && big[i])
+		{
+			if (is_match(((char *)(big + i)),(char *) little))
+				return ((char *)(big + l_len));
+		}
+		i++;
+	}
+	return (0);
+}
+int	main()
+{
+	char *big = "Foo Bar Baz";
+	char *little = "Foo";
+	
+	printf("%s", ft_strnstr(big, little, 3));
+}
