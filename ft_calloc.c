81be0aa6ae4d989de4fa5550b9e9104321b4dbd8
Yusuf Talha Sen <yussen@k1m12s03.42istanbul.com.tr>
2025-06-01 13:41:31 +0300

    updating general

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,22 +6,20 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 05:16:44 by yussen            #+#    #+#             */
-/*   Updated: 2025/06/01 13:11:35 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/01 13:39:54 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stdlib.h"
+#include "libft.h"
 
-void	*calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-	unsigned char *	res;
+	void *	res;
 	if (nmemb == 0 && size == 0)
 		return (NULL);
 	res = malloc(size * nmemb);
-	return (ft_bzero(res,nmemb));
-}
-
-int	main()
-{
-	
+	if (!res)
+		return (NULL);
+	ft_bzero(res,nmemb * size);
+	return (res);
 }
