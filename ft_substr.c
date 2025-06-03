77ccf1630c7295e4ba51afc96224e6dd4c95b8cf
Yusuf Talha Sen <yussen@student.42istanbul.com.tr>
2025-06-04 00:09:28 +0300

    first commit

---
+++ ft_substr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 11:30:08 by yussen            #+#    #+#             */
+/*   Updated: 2025/06/03 11:44:29 by yussen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*res;
+
+	i = 0;
+	res = malloc(len + 1);
+	if (!res)
+		return (NULL);
+	while (i < len)
+	{
+		res[i] = s[start + i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
