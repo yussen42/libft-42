ecca635fb703be7ec984a5b727d63f6d3aae0371
Yusuf Talha Sen <yussen@student.42istanbul.com.tr>
2025-06-15 00:46:53 +0300

    upgrading general

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 18:21:05 by yussen            #+#    #+#             */
-/*   Updated: 2025/06/03 23:52:22 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/15 00:42:13 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	base(int n)
 	int	res;
 
 	res = 0;
-	if (n < 0)
+	if (n <= 0)
 	{
 		res++;
 	}
@@ -29,26 +29,35 @@ static int	base(int n)
 	return (res);
 }
 
-static char *fill(int n, char *res, int i)
+static char	*fill(int n, char *res, int i)
 {
-	int t;
-	char tmp;
-	
-	t = i;
+	char	tmp;
+
+	if (n == 0)
+	{
+		res[i] = '0';
+		return (res);
+	}
 	if (n < 0)
 	{
 		res[0] = '-';
+		if (n == -2147483648)
+		{
+			res[i--] = '8';
+			n = -214748364;
+		}
 		n *= -1;
 	}
 	while (n)
 	{
 		tmp = (n % 10) + '0';
-		res[t] = tmp;
+		res[i] = tmp;
 		n = n / 10;
-		t--;
+		i--;
 	}
 	return (res);
 }
+
 char	*ft_itoa(int n)
 {
 	int		n_base;
@@ -59,11 +68,6 @@ char	*ft_itoa(int n)
 	if (!res)
 		return (NULL);
 	res[n_base] = '\0';
-	fill(n, res, n_base -1);
+	fill(n, res, n_base - 1);
 	return (res);
-}
-
-int	main()
-{
-	printf("%s\n", ft_itoa(-1234));
 }
