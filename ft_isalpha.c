f3affff0f56f5febda7345ed2025822a49318354
Yusuf Talha Sen <yussen@k1m15s01.42istanbul.com.tr>
2025-06-26 02:06:42 +0300

    final

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yussen <yussen@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 16:24:29 by yussen            #+#    #+#             */
-/*   Updated: 2025/05/28 16:30:59 by yussen           ###   ########.fr       */
+/*   Updated: 2025/05/29 17:10:34 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isalpha(int c)
 {
-	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
