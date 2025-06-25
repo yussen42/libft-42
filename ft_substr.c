cce97a3055f6f1b07efbca13f2d57e05db4d32a7
Yusuf Talha Sen <yussen@k1m15s01.42istanbul.com.tr>
2025-06-26 02:12:44 +0300

    final

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 11:30:08 by yussen            #+#    #+#             */
-/*   Updated: 2025/06/03 11:44:29 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/24 01:02:35 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	char	*res;
+	size_t	s_len;
 
-	i = 0;
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start > s_len)
+		return (ft_calloc(1, 1));
+	if (len > s_len - start)
+		len = s_len - start;
 	res = malloc(len + 1);
 	if (!res)
 		return (NULL);
-	while (i < len)
+	i = 0;
+	while (i < len && start + i < s_len)
 	{
 		res[i] = s[start + i];
 		i++;
