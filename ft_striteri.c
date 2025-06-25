a6d68367b98748612ed9ee21b14a62c8aab45cb4
Yusuf Talha Sen <yussen@student.42istanbul.com.tr>
2025-06-26 02:10:14 +0300

    final

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,22 +6,20 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 18:52:06 by yussen            #+#    #+#             */
-/*   Updated: 2025/06/13 05:02:44 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/22 19:37:13 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
 
-	i = 0;	
+	i = 0;
 	if (f)
 	{
 		while (s[i])
 		{
-			f(i,&s[i]);
+			f(i, &s[i]);
 			i++;
 		}
 	}
