48c75bc4c4c4ff7f35baad608bf841c0fd3ca0c0
Yusuf Talha Sen <yussen@student.42istanbul.com.tr>
2025-06-26 02:11:58 +0300

    final

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,22 +6,24 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 14:11:14 by yussen            #+#    #+#             */
-/*   Updated: 2025/06/01 16:57:03 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/24 00:37:13 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 
 	i = ft_strlen(s);
-	while (i > 0)
+	if (c == '\0')
+		return ((char *)s + ft_strlen(s));
+	while (i >= 0)
 	{
-		if (s == (char *)c)
-			return ((char *)s + i);
-		i++;
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i--;
 	}
-	return (0);
+	return (NULL);
 }
