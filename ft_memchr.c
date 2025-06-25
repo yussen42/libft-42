fc18693a7e8ae654192394ef82adfe3686621f7e
Yusuf Talha Sen <yussen@k1m15s01.42istanbul.com.tr>
2025-06-26 02:07:38 +0300

    final

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 12:54:59 by yussen            #+#    #+#             */
-/*   Updated: 2025/06/18 17:19:00 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/24 04:44:52 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,20 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	uc;
-	unsigned char	*str;
-	size_t			i;
+	const unsigned char	*str;
+	unsigned char		u_c;
+	size_t				i;
 
-	uc = (unsigned char)c;
-	str = (unsigned char *)s;
+	str = (const unsigned char *)s;
+	u_c = (unsigned char)c;
 	i = 0;
 	if (n == 0)
 		return (NULL);
-	if (c == '\0')
-		return (str + ft_strlen(s));
-	while (n)
+	while (i < n)
 	{
-		if (str[i] == uc)
-			return (str + i);
+		if (str[i] == u_c)
+			return ((void *)(&str[i]));
 		i++;
-		n--;
 	}
 	return (NULL);
 }
