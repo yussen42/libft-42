b44c0d0da2c16e56527685267a257cf7d6e63175
Yusuf Talha Sen <yussen@k1m15s01.42istanbul.com.tr>
2025-06-26 02:10:02 +0300

    final

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,27 +6,29 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 18:33:50 by yussen            #+#    #+#             */
-/*   Updated: 2025/05/28 18:41:07 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/21 13:58:02 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strdup(const char *s)
 {
 	int		i;
-	char	*dup;
+	char	*res;
 
 	i = 0;
 	while (s[i])
 		i++;
-	dup = malloc(sizeof(char) * (i + 1));
+	res = malloc(sizeof(char) * (i + 1));
+	if (!res)
+		return (0);
 	i = 0;
 	while (s[i])
 	{
-		dup[i] = s[i];
+		res[i] = s[i];
 		i++;
 	}
-	dup[i] = '\0';
-	return (dup);
+	res[i] = '\0';
+	return (res);
 }
