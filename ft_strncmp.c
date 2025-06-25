21e03fb6677879d8a14925f2b76eb214567747cf
Yusuf Talha Sen <yussen@k1m15s01.42istanbul.com.tr>
2025-06-26 02:11:22 +0300

    final

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,23 +6,22 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 20:23:07 by yussen            #+#    #+#             */
-/*   Updated: 2025/05/28 20:36:24 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/16 10:51:52 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && i < n)
+	while (i < n && (s1[i] || s2[i]))
 	{
-		if (s1[i] == s2[i])
-			i++;
-		else
-			return (s1[i] - s2[i]);
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
