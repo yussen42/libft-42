7c1ace6366b6a4b0cf8609e92829872cadf848a7
Yusuf Talha Sen <yussen@k1m15s01.42istanbul.com.tr>
2025-06-26 02:05:17 +0300

    final

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,15 +6,15 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 16:49:36 by yussen            #+#    #+#             */
-/*   Updated: 2025/05/30 17:07:41 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/22 04:40:37 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *nptr)
 {
-	int	i;
-	int	sign;
-	int	res;
+	int			i;
+	int			sign;
+	long int	res;
 
 	i = 0;
 	sign = 1;
@@ -32,5 +32,9 @@ int	ft_atoi(const char *nptr)
 		res = (res * 10) + (nptr[i] - '0');
 		i++;
 	}
+	if (res * sign > 2147483647)
+		return (-1);
+	if (res * sign < -2147483648)
+		return (0);
 	return (res * sign);
 }
