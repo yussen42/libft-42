6bdf3a68134648d738ce50ac7d082dc668813f5b
Yusuf Talha Sen <yussen@student.42istanbul.com.tr>
2025-06-26 02:07:54 +0300

    final

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 17:23:07 by yussen            #+#    #+#             */
-/*   Updated: 2025/06/18 20:31:16 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/22 19:21:47 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*d;
-	unsigned char	*s;
+	unsigned char		*d;
+	const unsigned char	*s;
 
 	if (!dest && !src)
 		return (NULL);
 	d = (unsigned char *)dest;
-	s = (unsigned char *)src;
+	s = (const unsigned char *)src;
 	if (s < d)
 	{
 		while (n)
 		{
-			d[n] = s[n];
 			n--;
+			d[n] = s[n];
 		}
 	}
 	else
