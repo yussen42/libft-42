ae0ed7fa3568c5fb5c064eafb37b7a9342aa4610
Yusuf Talha Sen <yussen@student.42istanbul.com.tr>
2025-06-26 02:11:09 +0300

    final

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,12 +6,11 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 16:20:48 by yussen            #+#    #+#             */
-/*   Updated: 2025/06/13 04:51:32 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/23 01:05:17 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
@@ -19,7 +18,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	size_t	len;
 	size_t	i;
 
-	if (!s)
+	if (!s || !f)
 		return (NULL);
 	i = 0;
 	len = 0;
@@ -35,24 +34,3 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	res[i] = '\0';
 	return (res);
 }
-/*
-char	c(unsigned int a, char b)
-{	
-	(void)a;
-	if (b >= 'a' && b <= 'z')
-		return (b - 32);
-	else
-		return (b);
-}
-int	main()
-{
-	unsigned int a = 5;
-	char b = 'A';
-	
-	char	res[20];
-	printf("%s", ft_strmapi(res, (*c)(a, b)));
-	char	str[] = "esduman";
-	char	*result = ft_strmapi(str, c);
-	printf("%s", result);
-	free(result);
-}*/
