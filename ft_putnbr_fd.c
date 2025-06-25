c095fba40186ff90de3c648ff8af1f16d96f6581
Yusuf Talha Sen <yussen@k1m15s01.42istanbul.com.tr>
2025-06-26 02:09:17 +0300

    final

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 13:44:14 by yussen            #+#    #+#             */
-/*   Updated: 2025/06/12 13:55:49 by yussen           ###   ########.fr       */
+/*   Updated: 2025/06/15 09:17:21 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
+	long	nbr;
+
+	nbr = (long)n;
+	if (nbr < 0)
 	{
 		ft_putchar_fd('-', fd);
-		if (n == -2147483647)
-		{
-			ft_putchar_fd('2', 1);
-			n = -147483647;
-		}
-		n *= -1;
+		nbr *= -1;
 	}
-	
-	if (n > 9)
+	if (nbr > 9)
 	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		ft_putnbr_fd(nbr / 10, fd);
+		ft_putnbr_fd(nbr % 10, fd);
 	}
-	if (n > -1 && n < 10)
-		ft_putchar_fd(n + 48,fd);
+	else
+		ft_putchar_fd(nbr + 48, fd);
 }
